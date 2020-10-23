7f29f4d88893bd548fd5c04ec33e9c28167d4651
danyi1998 <46101155+danyi1998@users.noreply.github.com>
2020-10-24 02:57:09 +0800

    Add files via upload

--- project/clustering.py
+++ project/clustering.py
@@ -9,6 +9,9 @@ df_discrete_values = df[["attacktype1", "targtype1", "weaptype1"]]
 
 
 '''
+
+# elbow method to find optimal number of clusters 
+
 cost = []
 for i in range(1,11):
     km = KModes(n_clusters=i, init = "Huang", n_init = 10)
@@ -20,6 +23,8 @@ x = np.array([i for i in range(1,11)])
 plt.plot(x,cost)
 
 plt.show()
+
+
 '''
 
 
@@ -37,29 +42,81 @@ df_clusters.columns = ["cluster"]
 
 df_with_clusters = pd.concat([df, df_clusters], axis = 1).reset_index()
 
-df_with_clusters.to_csv("df_with_clusters.csv")
 
 
 
-'''
-# not necessary, but just leave it here in case I need it in the future
-# check the code again if using
+
+cluster_0 = df_with_clusters[df_with_clusters["cluster"]==0] 
+
+cluster_0 = cluster_0["attacktype1_txt"] + ", " + cluster_0["targtype1_txt"] + ", " + cluster_0["weaptype1_txt"] 
+
+cluster_0 = cluster_0.groupby(cluster_0).count() 
+
+cluster_0 = cluster_0.sort_values(ascending = False)  
+
+cluster_0.to_csv("cluster_0.csv") 
 
 
-df_cluster_0 = df_with_clusters[df_with_clusters["cluster"]==0]
 
-fig = plt.figure()
 
-ax = fig.add_subplot(111, projection='3d')
+cluster_1 = df_with_clusters[df_with_clusters["cluster"]==1] 
 
-ax.scatter(df_cluster_0.attacktype1, df_cluster_0.weaptype1, df_cluster_0.targtype1, c="red", marker='o')
+cluster_1 = cluster_1["attacktype1_txt"] + ", " + cluster_1["targtype1_txt"] + ", " + cluster_1["weaptype1_txt"] 
 
-ax.set_xlabel("attack type")
-ax.set_ylabel("weapon type")
-ax.set_zlabel("target type")
+cluster_1 = cluster_1.groupby(cluster_1).count() 
 
-plt.show()
-'''
+cluster_1 = cluster_1.sort_values(ascending = False)  
+
+cluster_1.to_csv("cluster_1.csv") 
+
+
+
+
+cluster_2 = df_with_clusters[df_with_clusters["cluster"]==2] 
+
+cluster_2 = cluster_2["attacktype1_txt"] + ", " + cluster_2["targtype1_txt"] + ", " + cluster_2["weaptype1_txt"] 
+
+cluster_2 = cluster_2.groupby(cluster_2).count() 
+
+cluster_2 = cluster_2.sort_values(ascending = False)  
+
+cluster_2.to_csv("cluster_2.csv") 
+
+
+
+cluster_3 = df_with_clusters[df_with_clusters["cluster"]==3] 
+
+cluster_3 = cluster_3["attacktype1_txt"] + ", " + cluster_3["targtype1_txt"] + ", " + cluster_3["weaptype1_txt"] 
+
+cluster_3 = cluster_3.groupby(cluster_3).count() 
+
+cluster_3 = cluster_3.sort_values(ascending = False)  
+
+cluster_3.to_csv("cluster_3.csv") 
+
+
+
+cluster_4 = df_with_clusters[df_with_clusters["cluster"]==4] 
+
+cluster_4 = cluster_4["attacktype1_txt"] + ", " + cluster_4["targtype1_txt"] + ", " + cluster_4["weaptype1_txt"] 
+
+cluster_4 = cluster_4.groupby(cluster_4).count() 
+
+cluster_4 = cluster_4.sort_values(ascending = False)  
+
+cluster_4.to_csv("cluster_4.csv") 
+
+
+
+cluster_5 = df_with_clusters[df_with_clusters["cluster"]==5] 
+
+cluster_5 = cluster_5["attacktype1_txt"] + ", " + cluster_5["targtype1_txt"] + ", " + cluster_5["weaptype1_txt"] 
+
+cluster_5 = cluster_5.groupby(cluster_5).count() 
+
+cluster_5 = cluster_5.sort_values(ascending = False)  
+
+cluster_5.to_csv("cluster_5.csv") 
 
 
 
